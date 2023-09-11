ec4d42e3b6a108cf8e379ce0c310e501720494c6
NivekSotam <kevin.matoss.km@gmail.com>
2023-09-11 16:11:19 -0300

    feat: base layout

---
+++ src/components/SidebarWithHeader.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Box,
+  Drawer,
+  DrawerContent,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
+import SidebarContent from "./SideBar";
+import MobileNav from "./navbar";
+
+const SidebarWithHeader = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </DrawerContent>
+      </Drawer>
+
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Box>
+    </Box>
+  );
+};
+
+export default SidebarWithHeader;
